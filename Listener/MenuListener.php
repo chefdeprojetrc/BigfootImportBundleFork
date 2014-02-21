b2df3d0482fb4e6bb1118de931ed4e69e8e5270f
Guillaume Manen <g.manen@c2is.fr>
2014-02-21 12:08:18 +0100

    Menu access restricted to admin

--- Listener/MenuListener.php
+++ Listener/MenuListener.php
@@ -6,12 +6,26 @@ use Symfony\Component\EventDispatcher\GenericEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 use Bigfoot\Bundle\CoreBundle\Event\MenuEvent;
+use Symfony\Component\Security\Core\SecurityContextInterface;
 
 /**
  * Menu Listener
  */
 class MenuListener implements EventSubscriberInterface
 {
+    /**
+     * @var \Symfony\Component\Security\Core\SecurityContextInterface
+     */
+    private $security;
+
+    /**
+     * @param SecurityContextInterface $security
+     */
+    public function __construct(SecurityContextInterface $security)
+    {
+        $this->security = $security;
+    }
+
     /**
      * Get subscribed events
      *
@@ -32,21 +46,23 @@ class MenuListener implements EventSubscriberInterface
         $menu     = $event->getSubject();
         $fluxMenu = $menu->getChild('flux');
 
-        $fluxMenu->addChild(
-            'import',
-            array(
-                'label'  => 'Imports',
-                'route'  => 'admin_datasource',
-                'extras' => array(
-                    'routes' => array(
-                        'admin_datasource_new',
-                        'admin_datasource_edit'
+        if ($this->security->isGranted('ROLE_ADMIN')) {
+            $fluxMenu->addChild(
+                'import',
+                array(
+                    'label'  => 'Imports',
+                    'route'  => 'admin_datasource',
+                    'extras' => array(
+                        'routes' => array(
+                            'admin_datasource_new',
+                            'admin_datasource_edit'
+                        )
+                    ),
+                    'linkAttributes' => array(
+                        'icon' => 'level-down',
                     )
-                ),
-                'linkAttributes' => array(
-                    'icon' => 'level-down',
                 )
-            )
-        );
+            );
+        }
     }
 }
